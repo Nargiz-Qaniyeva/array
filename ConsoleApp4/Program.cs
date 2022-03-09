e3a72ed71d7f65f80213864f16bb05d177fa604e
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-10 01:21:36 +0400

    first commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+            {
+            int[] arr = { 1, 3, 4, 7, -9, 2, -5 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] < 0)
+
+                {
+                    int son = arr[i] * -1;
+
+                    Console.WriteLine(son);
+
+                }
+                //menfini musbet etdikden sonra, umumi massivi geri qaytara bilmirem(((;
+            }
+        }
+     
+        
+
+        }
+
+
+    }
+
